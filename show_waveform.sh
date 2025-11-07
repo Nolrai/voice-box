7be44cf839e107b145f419d93fbe5ed9bf91e053
Christina Upshaw <chrisaupshaw@gmail.com>
2025-11-07 09:33:21 -0800

    added comment and eof newline.

--- show_waveform.sh
+++ show_waveform.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+
+# Usage: ./show_waveform.sh filename.wav
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,3 +15,4 @@ plt.figure(figsize=(10,6))
 plt.subplot(2,1,1); plt.plot(t, data); plt.title('waveform'); plt.xlabel('s')
 plt.subplot(2,1,2); plt.specgram(data, NFFT=1024, Fs=sr, noverlap=512, cmap='magma'); plt.title('spectrogram'); plt.ylim(0, 4000)
 plt.tight_layout(); plt.show()
+
